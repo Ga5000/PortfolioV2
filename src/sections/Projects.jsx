2c1deb87fca78cced66eda420f0029f3c6b2fc28
Ga5000 <gbr.lisboa@gmail.com>
2025-01-13 12:50:25 -0300

    urls added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
                 ? "A Sudoku solver implemented using backtracking, with a user interface built in Java Swing for a simple and intuitive experience. The solver can efficiently solve any Sudoku puzzle, making use of recursion and backtracking algorithms." 
                 : "Um resolutor de Sudoku implementado usando retrocesso, com uma interface de usuário construída em Java Swing para uma experiência simples e intuitiva. O resolutor pode resolver qualquer quebra-cabeça de Sudoku de forma eficiente, utilizando algoritmos de recursão e retrocesso.",
             techStack: ["Java"],
-            githubUrl: "dmiqwndko", 
+            githubUrl: "https://github.com/Ga5000/SudokuSolver", 
         }, 
         { 
             title: language === 'en' ? "Library API" : "API de Biblioteca",
@@ -23,7 +23,7 @@ const Projects = () => {
                 ? "A simple library management API built with Java, Spring Boot, and MySQL. It supports basic CRUD operations for managing books, users, and transactions. Authentication is handled via JWT for secure access to endpoints." 
                 : "Uma API simples de gerenciamento de biblioteca construída com Java, Spring Boot e MySQL. Ela suporta operações básicas de CRUD para gerenciar livros, usuários e transações. A autenticação é realizada via JWT para acesso seguro aos endpoints.",
             techStack: ["Java", "MYSQL", "Spring Boot", "JWT"],
-            githubUrl: "dmiqwndko",
+            githubUrl: "https://github.com/Ga5000/Library-Rest-API",
         }, 
         { 
             title: language === 'en' ? "Clinic API" : "API de Clínica",
@@ -31,7 +31,7 @@ const Projects = () => {
                 ? "A clinic management API designed to handle basic functionalities such as appointment scheduling, patient records, and staff management. Built with Java, Spring Boot, and MySQL, the API ensures secure access through JWT authentication." 
                 : "Uma API de gerenciamento de clínica projetada para lidar com funcionalidades básicas, como agendamento de consultas, registros de pacientes e gerenciamento de funcionários. Construída com Java, Spring Boot e MySQL, a API garante acesso seguro por meio de autenticação JWT.",
             techStack: ["Java", "MYSQL", "Spring Boot", "JWT"],
-            githubUrl: "dmiqwndko", 
+            githubUrl: "https://github.com/Ga5000/Clinic", 
         },
     ];
 
